1e1bea1bf9cf6c17918384bccbf97a657fec9fe2
Angela Webb <angelawebb.dev@gmail.com>
2023-10-18 08:42:39 -0400

    adds 876

---
+++ challenge_876.java
@@ -0,0 +1,27 @@
+/* DESCRIPTION:
+ * Given the head of a singly linked list, return the middle node of the linked list.
+* If there are two middle nodes, return the second middle node.
+ */
+
+/* NOTES:
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode middleNode(ListNode head) {
+        ListNode middle = head;
+        ListNode end = head;
+
+        while(end!=null && end.next!=null){
+            middle = middle.next;
+            end = end.next.next;
+        }
+        return middle;
+    }
+}
