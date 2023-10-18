c2255fdef9261642cd3d78964f5aff01b256e34d
Angela Webb <angelawebb.dev@gmail.com>
2023-10-18 08:44:46 -0400

    adds 383

---
+++ challenge_383.java
@@ -0,0 +1,32 @@
+/* DESCRIPTION:
+* Given two strings ransomNote and magazine, 
+* return true if ransomNote can be constructed by using the letters from magazine and false otherwise.
+* Each letter in magazine can only be used once in ransomNote.
+ */
+
+class Solution {
+    public boolean canConstruct(String ransomNote, String magazine) {
+        int counter=0;
+        char[] charLetters = magazine.toCharArray();
+        ArrayList<String> letters = new ArrayList<String>();
+        for(int i=0;i<magazine.length();i++){
+            letters.add(String.valueOf(charLetters[i]));
+        }
+        for(int i=0;i<ransomNote.length();i++){
+            String ransomLetter = String.valueOf(ransomNote.charAt(i));
+            for(int j=0;j<letters.size();j++){
+                String zineLetter = letters.get(j);
+                if(zineLetter.equals(ransomLetter)){
+                    counter++;
+                    letters.remove(j);
+                    break;
+                }
+            }
+        }
+        if(counter==ransomNote.length()){
+            return true;
+        }else{
+            return false;
+        }
+    }
+}
